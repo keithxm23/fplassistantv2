2f246c4740e0f7a7a664dc23712f6452906bd8b3
Keith Mascarenhas <keithxm23@gmail.com>
2014-08-25 23:24:54 +0000

    added gradient colors to numbers: need to add to all

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -4,6 +4,29 @@ class PlayersController < ApplicationController
   def index
     @players = Player.where("status != 'u'").order("total_points DESC")
 
+    #TODO Refactor this to use raw select so that we're performing a single query
+    @vals = Player.select("MAX(now_cost) as max_cost," \
+                          "MIN(now_cost) as min_cost," \
+                          "MAX(total_points) as max_ts," \
+                          "MIN(total_points) as min_ts," \
+                          "MAX(minutes) as max_mins," \
+                          "MIN(minutes) as min_mins," \
+                          "MAX(form) as max_form," \
+                          "MIN(form) as min_form," \
+                          "MAX(ea_index) as max_ea_index," \
+                          "MIN(ea_index) as min_ea_index," \
+                          "MAX(form) as max_form," \
+                          "MIN(form) as min_form," \
+                          "MAX(form) as max_form," \
+                          "MIN(form) as min_form," \
+                          "MAX(form) as max_form," \
+                          "MIN(form) as min_form," \
+                          "MAX(form) as max_form," \
+                          "MIN(form) as min_form," \
+                          "MAX(form) as max_form," \
+                          "MIN(form) as min_form"
+                          )[0]
+
     jsondata = {}
     jsondata['total'] = "50"
     jsondata['page'] = "1"
