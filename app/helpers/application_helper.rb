b4c9618661e925ea486daf907bf393ca31be9ec8
Keith Mascarenhas <keithxm23@gmail.com>
2014-09-04 00:01:05 +0000

    disabling turbolinks via the link_to tags

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ module ApplicationHelper
     class_name = current_page?(link_path) ? 'selected' : nil
 
     content_tag(:li, :class => class_name) do
-      link_to link_text, link_path
+      link_to link_text, link_path, "data-no-turbolink" => true
     end
   end
 end
