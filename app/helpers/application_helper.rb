11db99ba3247a602fd8d0286193c8e64c833ef02
Keith Mascarenhas <keithxm23@gmail.com>
2014-09-11 20:48:32 +0000

    added rgbify in ruby

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,27 @@ module ApplicationHelper
       link_to link_text, link_path, "data-no-turbolink" => true
     end
   end
+
+  def rgbify(maxval, minval, val, moreisgood)
+    intnsty = 1.0*(val - minval) / (maxval - minval)
+    if moreisgood
+      if (intnsty > 0.5)
+          g = 255
+          r = (2 * (1 - intnsty) * 255).round
+      else
+          r = 255
+          g = (2 * intnsty * 255).round
+      end
+    else #lessisgood
+      if (intnsty > 0.5)
+          r = 255
+          g = (2 * (1 - intnsty) * 255).round
+       else
+          g = 255
+          r = (2 * intnsty * 255).round
+      end
+    end
+    return "rgb(" + r.to_s + ", " + g.to_s + ", 0)"
+  end
+
 end
